277ae9e877881747ba42fced7e1144af3fbfece1
Mirla Chucre <mirlabraga@gmail.com>
2020-10-30 10:21:03 +0000

    Add new registers on database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Products
+p1 = Product.create name:'Lavender heart', code:001, price:9.25
+p1.save
+
+p2 = Product.create name:'Personalised cufflinks', code:002, price:45
+p2.save
+
+p3 = Product.create name:'Kids T-shirt', code:003, price:19.95
+p3.save
+
+# Minimums
+m1 = Minimum.create category:0, value: 60 # 0 -> by value
+m1.save
+
+m2 = Minimum.create category:1, value: 2  # 1 -> by quantity
+m2.save
+
+# Discounts
+d1 = Discount.create category:0, value: 10 # 0 -> by percentage
+d1.save
+
+d2 = Discount.create category:1, value: 8.5  # 1 -> by valor
+d2.save
+
+# Rules
+rule = m1.rule.create!
